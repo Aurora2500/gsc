702370e8b66b15c62a665e6e3298b32dbb37b7ea
Aurora <programming.aurora@gmail.com>
2020-06-29 21:33:54 +0100

    Added keyboard camera movement as per #1

--- Camera.gd
+++ Camera.gd
@@ -1,13 +1,16 @@
 extends Camera2D
 
 # how far away the camera can move from the origin
-export var x_size = 2000
-export var y_size = 2000
+export var x_size = 1200
+export var y_size = 1200
 
-export var max_zoom = 4.0
+export var max_zoom = 2.0
 export var min_zoom = 0.4
 
-export var zoom_sensitivity = 1.2
+export var mouse_zoom_sensitivity = 1.2
+export var keyboard_zoom_sensitivity = 10
+
+export var keyboard_pan_sensitivity = 16
 
 var mouse_button_down: bool = false
 
@@ -17,9 +20,6 @@ var origin_mouse_pos: Vector2
 
 # keep track of the zoom
 var current_zoom: float = 1 
-
-func _ready():
-	print("oke")
 
 func _input(event):
 	### Dragging events
@@ -48,16 +48,34 @@ func _input(event):
 	if event is InputEventMouseButton:
 		# zoom in
 		if event.button_index == BUTTON_WHEEL_UP:
-			current_zoom /= zoom_sensitivity
-			
+			current_zoom /= mouse_zoom_sensitivity
+		# zoom out
 		if event.button_index == BUTTON_WHEEL_DOWN:
-			current_zoom *= zoom_sensitivity
-			
-		current_zoom = clamp(current_zoom, min_zoom, max_zoom)
-		zoom = Vector2(current_zoom, current_zoom)
+			current_zoom *= mouse_zoom_sensitivity
 		
-		
+		update_zoom()
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+
+### Keyboard control
+# keyboard keys are held down. So it needs to be managed frame by frame
+# instead of on a key event
+func _process(delta):
+	if Input.is_action_pressed("camera_up"):
+		position.y -= keyboard_pan_sensitivity
+	if Input.is_action_pressed("camera_down"):
+		position.y += keyboard_pan_sensitivity
+	if Input.is_action_pressed("camera_left"):
+		position.x -= keyboard_pan_sensitivity
+	if Input.is_action_pressed("camera_right"):
+		position.x += keyboard_pan_sensitivity
+	
+	if Input.is_action_pressed("camera_zoom_in"):
+		current_zoom -= keyboard_zoom_sensitivity * delta
+		update_zoom()
+	if Input.is_action_pressed("camera_zoom_out"):
+		current_zoom += keyboard_zoom_sensitivity * delta
+		update_zoom()
+
+func update_zoom():
+	current_zoom = clamp(current_zoom, min_zoom, max_zoom)
+	zoom = Vector2(current_zoom, current_zoom)
