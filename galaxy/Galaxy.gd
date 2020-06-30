f532e0fe14df0f281daee8457508df28bf6e6289
Aurora <programming.aurora@gmail.com>
2020-06-30 01:54:32 +0100

    Made placeholder SolarSystem node and scene and a basic generator
    The generator for now will just generate a number of given stars between a minimum and maximum radius

--- galaxy/Galaxy.gd
+++ galaxy/Galaxy.gd
@@ -4,9 +4,19 @@ export var number_of_stars = 100
 export var min_radius = 200
 export var max_radius = 1500
 
+var solar_system = preload("res://solar_system/SolarSystem.tscn")
 
 func _ready():
-	pass # Replace with function body.
+	generate_galaxy()
 
 func generate_galaxy():
-	pass
+	for _i in range(number_of_stars):
+		var dist = rand_range(min_radius, max_radius)
+		var rot = rand_range(0.0, TAU)
+		var coord = Vector2(
+			dist * cos(rot),
+			dist * sin(rot)
+		)
+		var current_star = solar_system.instance()
+		add_child(current_star)
+		current_star.position = coord
---
+++ solar_system/SolarSystem.gd
@@ -0,0 +1,6 @@
+extends Node2D
+
+class_name SolarSystem
+
+func _ready():
+	pass
